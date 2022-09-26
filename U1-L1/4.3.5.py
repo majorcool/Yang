e301208c0f5b3771ab65a805719ff24397ccb014
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-09-26 19:03:16 +0800

    1

--- U1-L1/4.3.5.py
+++ U1-L1/4.3.5.py
@@ -1,38 +1,18 @@
 # 4.3.5 改写 #4.2.5：函数内不打印菱形，将需要打印的菱形作为函数的返回值，用 2 种方式调用函数，传入不同的参数，打印 2 个不同的菱形
-def aaa(n=2):
-    n=int(n)
-    a = 0
-    b = " "
-    c = 0
-    d = 0
-    if n != 0:
-        if n >= 2:
-            if n % 2 != 0:
-                a = (n - 1) / 2
-                d = 1
-            elif n % 2 == 0:
-                a = (n - 2) / 2
-                d = 2
-        # elif n==1:
-        # print("*")
-        elif n == 0:
-            return""
+a=0(不知问题所在)
+b=0
+r=0
+z=0
+def nnn(n):
+    n = int(n)
+    if n % 2 != 0:
+        a = (n + 1) / 2
         a = int(a)
-        b = a
-        for a in range(a, -1, -1):
-            return " " * a
-            c = (b - a) * 2 + 1
-            return "*" * c
-        a = b
-        if d == 1:
-            for a in range(1, a + 1):
-                return " " * a
-                c = (b - a) * 2 + 1
-                return "*" * c
-        if d == 2:
-            return "*" * (n - 1)
-            for a in range(1, a + 1):
-                return " " * a
-                c = (b - a) * 2 + 1
-                return "*" * c
-print(aaa(8))
+        for b in range(1, a + 1):
+            r= (r+"\n")+(" " * (a - b) + "*" * (2 * b - 1))
+        c = a - 1
+        for d in range(1, c + 1):
+            z=(z+"\n")+( " " * d + "*" * (n-2 * d))
+        return r+"\n"+z
+x=nnn(7)
+print(x)

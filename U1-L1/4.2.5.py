b5d650d7751aaf1a422a8f4a79ff9d846150ae28
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-09-26 18:23:29 +0800

    1

--- U1-L1/4.2.5.py
+++ U1-L1/4.2.5.py
@@ -1,38 +1,15 @@
 # 4.2.5 参考上节课练习 #3.3.2 的打印菱形，定义一个函数，能够根据不同的参数打印不同大小的菱形
-def aaa(n=2):
+b=0
+def aaa(n):
     n=int(n)
-    a = 0
-    b = " "
-    c = 0
-    d = 0
-    if n != 0:
-        if n >= 2:
-            if n % 2 != 0:
-                a = (n - 1) / 2
-                d = 1
-            elif n % 2 == 0:
-                a = (n - 2) / 2
-                d = 2
-        # elif n==1:
-        # print("*")
-        elif n == 0:
-            print("")
-        a = int(a)
-        b = a
-        for a in range(a, -1, -1):
-            print(" " * a, end="")
-            c = (b - a) * 2 + 1
-            print("*" * c)
-        a = b
-        if d == 1:
-            for a in range(1, a + 1):
-                print(" " * a, end="")
-                c = (b - a) * 2 + 1
-                print("*" * c)
-        if d == 2:
-            print("*" * (n - 1))
-            for a in range(1, a + 1):
-                print(" " * a, end="")
-                c = (b - a) * 2 + 1
-                print("*" * c)
-aaa("8")
+    if n%2!=0:
+        a=(n+1)/2
+        a=int(a)
+        for b in range(1,a+1):
+            print(" "*(a-b),end="")
+            print("*" * (2 * b - 1))
+        c=a-1
+        for d in range(1,c+1):
+            print(" "*d,end="")
+            print("*" * (n-2*d))
+aaa("7")

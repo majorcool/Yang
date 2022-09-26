d44fc8b95a7877b0958a2535084f5a8df02b0a0a
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-09-26 17:52:49 +0800

    1

--- U1-L1/3.3.3.py
+++ U1-L1/3.3.3.py
@@ -1,2 +1,13 @@
 # 3.3.3 按照下列格式打印九九乘法表
-(不会)
+n=0
+for a in range(1,10):
+    if a>=1:
+        print("\n")
+        for b in range(1,a+1):
+            n=a*b
+            print(a,end="")
+            print(" * ",end="")
+            print(b,end=" = ")
+            print(n,end="  ")
+
+

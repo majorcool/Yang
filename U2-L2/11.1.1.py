1a93d4aa6acaf4dbe53a244023193586020dca11
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-10-27 12:18:31 +0800

    submit

--- U2-L2/11.1.1.py
+++ U2-L2/11.1.1.py
@@ -1,18 +1,29 @@
 class Student:
-    def __init__(self):
-        self.name=input("name:")
-        self.ID=input("ID:")
-        self.course=input("course(用逗号隔开):").split(",")
+    def __init__(self, name, ID, course):
+        self.name = name
+        self.ID = ID
+        self.course = course.split(",")
+
+    def __del__(self):
+        print("已删除学生：{}   {}".format(self.name, self.ID))
+
+    def __len__(self):#不知为何不对
+        return len(self.course)
+
+    def __str__(self):
+        return self.name
+
+    def print(self):
+        print("{} {}".format(self.name, self.ID))
+
     def get_course(self):
         print(self.course)
-    def __len__(self):
-        print("已选{}节课".format(len(self.course)))
-    def print(self):
-        print("{} {}".format(self.name,self.ID))
-    def __del__(self):
-        print("已删除学生：{}   {}".format(self.name,self.ID))
-student_1=Student()
+
+
+student_1 = Student("kun", "1234567", "PE,Math,English")
 student_1.print()
 student_1.get_course()
-len(student_1.course)
+print(student_1.course)
+print(len(student_1))
+print(student_1)
 del student_1

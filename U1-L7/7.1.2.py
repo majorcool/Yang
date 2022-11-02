d12dc01ec68413aa0bcae69065bd5b3268eaa5ee
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-11-02 20:53:07 +0800

    submit

--- U1-L7/7.1.2.py
+++ U1-L7/7.1.2.py
@@ -2,15 +2,14 @@
 #哈哈数的定义如下：对于一个正整数，计算每一位数字的平方，求和，得到一个新的正整数
 #对于新的正整数，重复这个过程，直到和变为 1，也可能是无限循环但始终不为 1。
 #如果最终和为 1，那么这个数就是哈哈数，返回 True，否则返回 False
-num=input("num")
-while 1:
-    num = " ".join(num)#不知为何错
-    num = num.split(" ")
-    sum = 0
-    for n in range(0, len(num)):
-        num2=int(num[n])
-        num[n] = num2 * num2
-        sum += num[n]
-    print(num)
-    if sum == 1:
-        break
+def Y(n):
+    a = 0
+    for i in str(n):
+        a = a + int(i) ** 2
+    if a == 1:
+        return True
+    else:
+        return False
+
+
+print(Y(19))

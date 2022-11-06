40749046c900e3fa14e533bd686e56103c290bbf
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-11-06 15:42:38 +0800

    submit

---
+++ exam6/1.py
@@ -0,0 +1,10 @@
+#定义一个函数 is_power_of_3_v1(n)，参数为整数，判断 n 是否为 3 的幂
+#定义另一个函数 is_power_of_3_v2(n)，用另一种方式，判断 n 是否为 3 的幂
+def is_power_of_3_v1(n):
+    if n==1:
+        return True
+    if n%3==0:
+        return is_power_of_3_v1(n/3)
+    else:
+        return False
+print(is_power_of_3_v1(12))

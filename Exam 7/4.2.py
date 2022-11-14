1874678c9b7d1517860ea9c2c9226fbc19b4f30d
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-11-14 21:49:45 +0800

    submit

--- Exam 7/4.2.py
+++ Exam 7/4.2.py
@@ -1,10 +1,14 @@
 class Animal:
-    def __init__(self, health):
+    list1 = []
+
+    def __init__(self, health, name):
         self.health = health
+        self.name = name
+        self.list1.append(self.name)
 
     def increase(self, n):
         self.health = self.health + n
-        return self.health
+        print(self.health)
 
 
 class Manager:
@@ -22,9 +26,32 @@ class Manager:
 
 
 class Keeper(Manager):
-    def __init__(self, n:Animal, v):
-        self.performance = v
-        if n.health < 80:
+    def __init__(self, v:Animal, n):
+        super(Keeper, self).__init__(Manager)
+        self.health = n
+        if v.health < 80:
             self.performance -= 20
 
+    def feed(self, n:Animal):
+        Animal.increase(n, 20)
+
+
 class Trainer(Manager):
+    def __init__(self, v:Animal, n):
+        super(Trainer, self).__init__(Manager)
+        self.health = n
+        if v.health < 80:
+            self.performance -= 20
+
+    def perform(self ,n:Animal):
+        Animal.increase(n, -20)
+
+def everyday():
+    dog = Animal(100, "dog")
+    m = Manager(100)
+    m.feed(dog)
+    k = Keeper(dog, 90)
+    k.feed(dog)
+    t = Trainer(dog, 90)
+    t.perform(dog)
+everyday()

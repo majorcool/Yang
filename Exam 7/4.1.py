d5aa7798c55b4533e76f9aba9b451a522f434710
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-11-15 08:00:43 +0800

    submit

--- Exam 7/4.1.py
+++ Exam 7/4.1.py
@@ -1,14 +1,22 @@
-def gcd_pro_max_2(* n:int):
-    list1=[]
+def gcd_pro_max_2(n:list):
     while 1:
         for a in range(0,len(n)-1):
             if n[a] % n[a+1] == 0:
-                list1.append(n[a+1])
-                list1.append(n[a+1])
+                n.pop(a)
+                n.append(n[a+1])
+                n.pop(a+1)
+                n.append(n[a+1])
             else:
                 b = n[a] % n[a+1]
-                list1.append(b)
-                list1.append(n[a+1])
-        for b in list1:
-
-gcd_pro_max_2(122333)
+                n.pop(a)
+                n.append(b)
+                n.pop(a+1)
+                n.append(n[a+1])
+        count = 0
+        for b in range(0,len(n)-1):
+            if n[b] == n[b+1]:
+                count =+ 1
+        if count == len(n)-1:
+            break
+    print(n[0])
+gcd_pro_max_2([15,6,9])

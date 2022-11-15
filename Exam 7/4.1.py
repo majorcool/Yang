5b3c47b7f9241e501f280117e31dd60bd2629d1a
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-11-15 09:30:37 +0800

    submit

--- Exam 7/4.1.py
+++ Exam 7/4.1.py
@@ -1,22 +1,24 @@
-def gcd_pro_max_2(n:list):
-    while 1:
-        for a in range(0,len(n)-1):
-            if n[a] % n[a+1] == 0:
-                n.pop(a)
-                n.append(n[a+1])
-                n.pop(a+1)
-                n.append(n[a+1])
+def gcd_pro_max_2(* nums):
+    nums = sorted(nums, reverse=True)
+    if len(set(nums)) == 1:
+        return (set(nums)).pop()
+    for i in range(len(nums)-1):
+        if nums[i] % nums[i+1] == 0:
+            nums[i] = nums[i+1]
+        else:
+            nums[i] = nums[i] % nums[i+1]
+    return gcd_pro_max_2(* nums)
+
+print(gcd_pro_max_2(999,1999,2999,3999,4999))
+
+def gcd_pro_max_1(* nums):
+    while len(set(nums)) > 1:
+        nums = sorted(nums, reverse=True)
+        for i in range(0, len(nums) - 1):
+            if nums[i] % nums[i + 1] == 0:
+                nums[i] = nums[i + 1]
             else:
-                b = n[a] % n[a+1]
-                n.pop(a)
-                n.append(b)
-                n.pop(a+1)
-                n.append(n[a+1])
-        count = 0
-        for b in range(0,len(n)-1):
-            if n[b] == n[b+1]:
-                count =+ 1
-        if count == len(n)-1:
-            break
-    print(n[0])
-gcd_pro_max_2([15,6,9])
+                nums[i] = nums[i] % nums[i + 1]
+    print(set(nums).pop())
+
+gcd_pro_max_1(999,1999,2999,3999,4999)

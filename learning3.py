712b0eb08928dd7e874e52f10eabc0aa790f2c73
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-11-17 17:51:58 +0800

    submit

--- learning3.py
+++ learning3.py
@@ -1,10 +1,38 @@
-a = 9
-while a>=0 :
-    b = 1
-    while b<=a  :
-        print("*",end="")
-        b=b+1
-    a=a-1
-    print("")
+class List:
+    def __init__(self):
+        self.order = []
 
+    def add(self, n):
+        self.order.append(n)
 
+    def delate(self):
+        self.order.pop(0)
+
+    def isempty(self):
+        if len(self.order) == 0:
+            return True
+        else:
+            return False
+
+    def popmethod(self):
+        self.order.pop()
+
+    def max(self):
+        o = -9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+        for i in self.order:
+            if i > o:
+                o = i
+        return o
+
+    def mini(self):
+        o = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+        for i in self.order:
+            if i < o:
+                o = i
+        return o
+
+    def first(self):
+        print(self.order[0])
+
+    def lenth(self):
+        return len(self.order)

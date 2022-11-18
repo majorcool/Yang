03b8507aa71f99b209c4892fb7606db87ef3eeba
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-11-18 11:23:55 +0800

    submit

--- U2-L6/16.3.1.py
+++ U2-L6/16.3.1.py
@@ -14,18 +14,18 @@ class Rule:
     def __init__(self):
         self.live = 9
 
-    def increase(self, num):
-        self.live += num
+    def increase(self, livepeople):
+        self.live = len(livepeople)
 
-    def out(self, livepeople):
+    def out(self, livepeople, rule):
         list1 = []
-        repeat = 0
         count = 0
         output = 0
         for i in livepeople:
-            list1.append(i.vote(self))
+            list1.append(random.randint(0, rule.live-1))
         list1 = sorted(list1, reverse=True)
         for i in list1:
+            repeat = 0
             for z in list1:
                 if i == z:
                     repeat += 1
@@ -67,11 +67,12 @@ def game():
     for i in range(0, 3):
         killer.append(Mafia())
     for i in range(0, 6):
-        goodpeople.append(People)
-        livepeople.append(People)
-    for l in range(0, 3):
-        livepeople.append(killer[l])
+        goodpeople.append(People())
 
+    # for l in range(0, 3):
+    #     livepeople.append(killer[l])
+
+    livepeople = goodpeople + killer
     for i in killer:
         ticket.append(i.kill(rule))
     ticket = sorted(ticket, reverse=True)
@@ -86,28 +87,31 @@ def game():
     print(livepeople)
     print(goodpeople)
     print(killer)
+
     while len(goodpeople) > 0 and len(killer) > 0:
         if livepeople[kill_person] in goodpeople:
             print(livepeople[kill_person])
             goodpeople.remove(livepeople[kill_person])
         if livepeople[kill_person] in killer:
-            killer.remove(livepeople[kill_person])
+            killer.pop(kill_person- 6)
         livepeople.pop(kill_person)
-        rule.increase(-1)
+        rule.increase(livepeople)
+        print(len(livepeople))
         print(len(goodpeople))
         print(len(killer))
-        print()
+        print("kill")
 
-        voted_people = rule.out(livepeople)
+        voted_people = rule.out(livepeople, rule)
         if livepeople[voted_people] in goodpeople:
+            print(livepeople[voted_people])
             goodpeople.remove(livepeople[voted_people])
         if livepeople[voted_people] in killer:
-            killer.remove(livepeople[voted_people])
-        livepeople.pop(voted_people)
-        rule.increase(-1)
+            killer.pop(kill_person - 6)
+        rule.increase(livepeople)
+        print(len(livepeople))
         print(len(goodpeople))
         print(len(killer))
-        print()
+        print("vote")
 
     rule.victory(goodpeople)
 

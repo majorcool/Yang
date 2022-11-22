f5904a9b33e1b8e0fb42fecbbfc3863f9288cd09
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-11-22 17:28:54 +0800

    submit

--- 导论/二叉树.py
+++ 导论/二叉树.py
@@ -7,38 +7,38 @@ class Child:
             return 1
         if n < 12:
             return 0
-        if n == 0:
+        if n == 12:
             return '[0]'
 
 
 class LeftChild(Child):
-    layer = {1: [5, 5], 2: [3, 5]}
+    layer = {1: [5, 5], 2: [3, 5], 3: [-99999999999, 5]}
 
     def add(self, count):
         counts = 1
         while 1:
-            if count in LeftChild.layer:
-                if count < LeftChild.layer[count][0]:
+            if counts in LeftChild.layer:
+                if count < LeftChild.layer[counts][0]:
                     counts += 1
-                elif count == LeftChild.layer[count][0]:
-                    return '[0]' * count
+                elif count == LeftChild.layer[counts][0]:
+                    return '[0]' * counts
                 else:
-                    return '[0]' * count + '[1]'
+                    return '[0]' * counts
 
 
 class RightChild(Child):
-    layer = {1: [5, 15], 2: [3, 16]}
+    layer = {1: [5, 15], 2: [5, 16], 3: [5, 99999999999999999999]}
 
     def add(self, count):
         counts = 1
         while 1:
-            if count in RightChild.layer:
-                if count > RightChild.layer[count][1]:
+            if counts in RightChild.layer:
+                if count > RightChild.layer[counts][1]:
                     counts += 1
-                elif count == RightChild.layer[count][1]:
-                    return '[1]' * count
+                elif count == RightChild.layer[counts][1]:
+                    return '[1]' * counts
                 else:
-                    return '[1]' * count + '[0]'
+                    return '[1]' * counts
 
 
 def test(n):
@@ -54,4 +54,4 @@ def test(n):
         print(choose)
 
 
-test(12)
+test(1)

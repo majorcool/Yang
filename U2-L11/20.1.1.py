fd65427eb96f140c011e33103397e31e30d04579
yangzhen <zhen.yang.2201@stu.moonshotacademy.cn>
2022-11-29 10:29:36 +0800

    submit

---
+++ U2-L11/20.1.1.py
@@ -0,0 +1,7 @@
+# 20.1.1  Module & Package
+# 1. 新建一个 equation  包
+# 2. 在目录下，新建两个模块 conic 和 liner
+# 3. 在 conic 模块中定义一个 Circle 类 和 Ellipse 类，Circle 继承自 Ellipse
+# 4. 在 Circle 类中定义 perimeter 方法和 area 方法
+# 5. 在 receive_message 文件中定义一个 receive 函数
+# 6. 新建 test.py，直接导入 equation 包
---
+++ exam4/extra2.py
@@ -0,0 +1,7 @@
+"""def xxx(s):
+    a=len(s.split().pop())
+    return s[-a:]
+print(xxx("xxxxxxxxxx x"))"""
+str1 = "this is string example....wow!!!"
+str1=str1.translate(" "," ","s")
+print(str1.translate(" "," ",'s'))
